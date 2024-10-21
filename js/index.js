49c13886b1896a9a9cbf16e150293db2e1ad7dcd
Pablo <pablo.rotela@gmail.com>
2024-10-21 01:22:04 -0300

    doble login, login del Admin y acceso a Página Admin

--- js/index.js
+++ js/index.js
@@ -1,11 +1,7 @@
-const nombreDelUsuario = document.getElementById("nombreUsuario");
-const divDePassword = document.getElementById("divDePass");
-const passUsuario = document.getElementById("passwordUsuario");
-const mensaje = document.getElementById("passError");
 
 function EvaluarAdmin()
 {
-    window.location.href = "./html/administrador.html";
+    window.location.href = "./html/loginAdmin.html";
 }
 
 
